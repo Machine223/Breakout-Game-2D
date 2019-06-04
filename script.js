8ec01f3f8e6ac2b84094e7473a6d040e57b30d86
Machine <abderrahim223@gmail.com>
2019-06-04 16:27:12 -0400

    adding html code

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
-var canvas = document.getElementById('canvas');
+/* Ici on donne la référence à Javascript de canvas*/
+var canvas = document.getElementById('myCanvas');
+/* la variable ctx est créer pour stocker le contexte de rendu 2D*/
 var ctx = canvas.getContext('2d');
+/*getContext Est un DOMString contenant l'identifcateur de contexte définissant le contexte de dessin associé au canevas. 
+Les valeurs possibles sont :
+    1. 2d", conduisant à la création d'un objet CanvasRenderingContext2D représentant 
+    un contexte de représentation bi-dimensionnel.
+    2. "webgl" (ou "experimental-webgl") pour créer un objet WebGLRenderingContext représentant un contexte de représentation tri-dimensionnel. Ce contexte est seulement disponible sur les 
+    navigateurs implémentant la version 1 de WebGL (OpenGL ES 2.0).
+    3. "webgl2" pour créer un objet WebGL2RenderingContext représentant un contexte 
+    de représentation tri-dimensionnel. Ce contexte est seulement disponible sur les navigateurs implémentant la version 2 de WebGL (OpenGL ES 3.0). .
+    4. "bitmaprenderer" pour créer un ImageBitmapRenderingContext ne fournissant que la
+    fonctionnalité de remplacement du contenu du canevas par une ImageBitmap donnée. */
+
+//L'algorithme utilisé pour déterminer si un point est 
+// à l'intérieur ou à l'extérieur du chemin.
+
+ctx.beginPath(); //commencer un nouveau chemin
+ctx.rect(20, 40, 50, 50); // rectangle de dimension 
+ctx.fillStyle = "black"; // couleur 
+ctx.fill(); // renplir l'objet
+ctx.closePath(); // fin du chemin
+
+ctx.beginPath(); // nouveau chemin
+ctx.arc(240, 160, 20, 0, Math.PI*2, false);
+ctx.fillStyle = "green";
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.rect(160, 10, 100, 40);
+ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+ctx.stroke(); //style de trait seulement vide a l'int.
+ctx.closePath();
